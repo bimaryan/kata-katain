02961b7be01656e867a778574c56d63f5690fa72
bimaryan <74827383+bimaryan@users.noreply.github.com>
2023-01-01 09:04:25 +0700

    Update NotFound.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,12 @@
 const NotFound = () => {
   return (
-    <div className="centered">
-      <p>Page not found!</p>
+    <div class="card">
+      <div class="card-body text-center bg-danger text-light">
+        <h1>404</h1>
+        <h4>ERROR</h4>
+        <p>Hayooo mau ngapain???</p>
+        <a href="/" className="btn btn-info">Back To Home</a>
+      </div>
     </div>
   );
 };
