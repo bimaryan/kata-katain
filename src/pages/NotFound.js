b67cdcd66c9730ea7f4d12ba6a41d8e6112da3f2
bimaryan <74827383+bimaryan@users.noreply.github.com>
2023-01-01 08:27:58 +0700

    Update NotFound.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,7 @@
-import ParticlesBg from 'particles-bg'
-
 const NotFound = () => {
   return (
     <div className="centered">
       <p>Page not found!</p>
-      <ParticlesBg type="think" bg={true} />
     </div>
   );
 };
